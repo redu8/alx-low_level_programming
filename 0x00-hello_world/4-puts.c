6465cd56951e72a174fb05f7d544a79447f30e26
redu8 <Ridethatlaba8@gmail.com>
2023-03-19 11:43:34 -0700

    my c project

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-
 #include<stdio.h>
 /**
  * main - Entry point
@@ -7,8 +6,6 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
-
-
