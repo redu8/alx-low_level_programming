f24ff1690a21d5d244b5222b8160eb7a1b2bf2e6
redu8 <Ridethatlaba8@gmail.com>
2023-03-24 10:06:41 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,17 +14,21 @@ int i;
 for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 != 0)
-
+{
 printf(" Fizz");
-else if (i % 5 == 0 && i % 3 != 0)
 }
+else if (i % 5 == 0 && i % 3 != 0)
+{
 printf(" Buzz");
+}
 else if (i % 3 == 0 && i % 5 == 0)
 {
 printf(" FizzBuzz");
+}
 else if (i == 1)
 {
 printf("%d", i);
+}
 else
 {
 printf(" %d", i);
@@ -34,5 +38,5 @@ printf("\n");
 
 return (0);
 }
-}
+
 
